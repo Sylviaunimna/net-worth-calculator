41d3ed295c61e9de3291674b9bf1e0aeab5febb1
Sylviaunimna <sylviaunimna@yahoo.com>
2021-12-11 12:10:28 -0700

    added net-worth-calculator app

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Calculator from "./components/calculator";
+
+ReactDOM.render(<Calculator />, document.getElementById("root"));
